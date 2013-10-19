1efd1b1d09a5a3137fc7d0e0b082221281eb387f
Lawrence Luk <lluk@ucsd.edu>
2013-10-18 20:19:56 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(".incbutton").click( function () {
+	$("#worrrttthhh").html(options[Math.round(Math.random()*options.length)]);
+});
+
+var options = new Array("WORTH", "Totally Worth", "Completely Worth", "Undoubtably Worth", "Wooorrrttthhh", "Unquestionably Worth",
+	"Easily Worth", "So Worth", "No Regrets", "My Life For Aiur", "Absolutely Worth", "Drop Bows On Em", "GOT EM", "Extremely Worth",
+	"할 보람 있는", "I'd do it again", "Eff yeah", "Hell yeah", "Yah boi", "Yah gurl", "YOU BETCHA", "No Doubt", "So Easy");
